7f72f02e92a2ae703e63c8792d4ebe420d574983
eltonsky <eltonsky9404@gmail.com>
2014-04-22 22:38:03 +1000

    add db script 1_1

---
+++ db/1_1_schema_update.sql
@@ -0,0 +1,153 @@
+
+CREATE DATABASE IF NOT EXISTS rea;
+
+use rea;
+
+# taddr
+CREATE TABLE IF NOT EXISTS taddr (
+  addr_id 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  street_no    VARCHAR(16),
+  street_name  VARCHAR(64) NOT NULL, # can be "POA","Contact Agent"
+  INDEX street_name_ix (street_name),
+
+  locality     VARCHAR(64),
+  INDEX locality_ix (locality),
+
+  region       VARCHAR(16),
+  INDEX region_ix(region),
+
+  postcode     VARCHAR(4), 
+  INDEX postcode_ix(postcode),
+
+  UNIQUE KEY addr_unique_ix (street_no,street_name,locality,region),
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tproperty
+CREATE TABLE IF NOT EXISTS tproperty (
+  prop_id 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  ext_id  	INT,
+  addr_id 	INT NOT NULL,
+  FOREIGN KEY (addr_id) 
+        REFERENCES taddr(addr_id),
+  type    	VARCHAR(32),
+  INDEX type_ix(type),
+
+  land_size VARCHAR(128),
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tsale
+CREATE TABLE IF NOT EXISTS tsale (
+  sale_id 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  prop_id   INT NOT NULL,
+  FOREIGN KEY (prop_id) 
+        REFERENCES tproperty(prop_id),
+
+  status    VARCHAR(32) NOT NULL DEFAULT "LISTED",
+  INDEX status_ix(status),
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tprice
+CREATE TABLE IF NOT EXISTS tprice (
+  sale_id 	INT NOT NULL,
+  FOREIGN KEY (sale_id) 
+        REFERENCES tsale(sale_id),
+  INDEX sale_id_ix(sale_id),
+
+  price    VARCHAR(32) NOT NULL,
+  type     VARCHAR(16),
+  INDEX type_ix(type),
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tfeature
+CREATE TABLE IF NOT EXISTS tfeature (
+  sale_id 	INT NOT NULL,
+  FOREIGN KEY (sale_id) 
+        REFERENCES tsale(sale_id),
+  INDEX sale_id_ix(sale_id),
+
+  bed       INT,
+  INDEX bed_ix(bed),
+
+  bath      INT,
+  INDEX bath_ix(bath),
+
+  car_space INT,
+  INDEX car_space_ix(car_space),
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tsalestatus
+CREATE TABLE IF NOT EXISTS tsalestatus (
+  sale_id 	INT NOT NULL,
+  FOREIGN KEY (sale_id) 
+        REFERENCES tsale(sale_id),
+  INDEX sale_id_ix(sale_id),
+
+  status    VARCHAR(32) NOT NULL,   
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tinspection
+CREATE TABLE IF NOT EXISTS tinspection (
+  sale_id 	INT NOT NULL,
+  FOREIGN KEY (sale_id) 
+        REFERENCES tsale(sale_id),
+  INDEX sale_id_ix(sale_id),
+
+  start    VARCHAR(32) NOT NULL,
+  end      VARCHAR(32) NOT NULL,
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tagentcy
+CREATE TABLE IF NOT EXISTS tagency (
+  agency_id 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name      VARCHAR(32) NOT NULL,
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+
+# tagent
+CREATE TABLE IF NOT EXISTS tagent (
+  agent_id 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name      VARCHAR(32) NOT NULL,
+  phone     VARCHAR(32),
+  agency_id INT,
+  FOREIGN KEY (agency_id) 
+        REFERENCES tagency(agency_id), 
+
+  cr_date 	TIMESTAMP DEFAULT NOW()
+) ENGINE=INNODB ;
+
+
+# tpropertyagent
+CREATE TABLE IF NOT EXISTS tpropertyagent (
+  agent_id 	INT NOT NULL,
+  prop_id   INT NOT NULL,
+  FOREIGN KEY (agent_id) 
+        REFERENCES tagent(agent_id), 
+  INDEX agent_id_ix(agent_id),
+  FOREIGN KEY (prop_id) 
+        REFERENCES tproperty(prop_id), 
+  INDEX prop_id_ix(prop_id)
+) ENGINE=INNODB ;
+
+
